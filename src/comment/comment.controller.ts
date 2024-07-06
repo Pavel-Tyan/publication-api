e67b8b2e1e0101b985bde4eea6a18f5e77f83678
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-06 13:13:30 +0300

    feat: implement comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,18 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
 import { CommentModel } from './comment.model';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { CommentService } from './comment.service';
+import { REVIEW_NOT_FOUND } from './comment.constants';
 
 @Controller('comment')
 export class CommentController {
+    constructor(private readonly commentService: CommentService) {}
+
     @Post('create')
-    async create(@Body() dto: Omit<CommentModel, '_id'>) {}
+    async create(@Body() dto: CreateCommentDto): Promise<CommentModel> {
+        return this.commentService.create(dto);
+    }
 
     @Get('byPublication/:publicationId')
-    async getByPublication() {}
+    async getByPublication(): Promise<CommentModel[]> {
+        return this.commentService.findByPublicationId('publicationId');
+    }
 
-    // @Delete(':id')
-    // async delete(@Param('id') id: string) {}
+    @Delete(':id')
+    async delete(@Param('id') id: string): Promise<void> {
+        const deletedDoc = await this.commentService.delete(id);
 
-    @Patch(':id')
-    async delete(@Param('id') id: string, @Body() dto: CommentModel) {}
+        if (!deletedDoc) {
+            throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+        }
+    }
 }
