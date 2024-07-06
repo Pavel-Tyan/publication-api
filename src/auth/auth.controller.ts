72a599254981ad403b2e41becaff55d9880ae773
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-06 17:30:18 +0300

    feat: implement register logic in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import { AuthService } from './auth.service';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @UsePipes(new ValidationPipe())
     @Post('register')
-    async register(@Body() dto: UserDto) {}
+    async register(@Body() dto: UserDto) {
+        const oldUser = await this.authService.findUser(dto.email);
+
+        if (oldUser) {
+            throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+        }
+
+        return this.authService.createUser(dto);
+    }
 
     @HttpCode(200)
     @Post('login')
