2c3d76c1267995dc64f5e3bd6b68589ffa408df4
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-07 20:24:24 +0300

    docs: add documentation for publication module

--- src/publication/dto/create-publication.dto.ts
+++ src/publication/dto/create-publication.dto.ts
@@ -1,12 +1,17 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePublicationDto {
+    @ApiProperty({ description: 'Category of publication', nullable: false })
     @IsString()
     category: string;
+    @ApiProperty({ description: 'Title of publication', nullable: false })
     @IsString()
     title: string;
+    @ApiProperty({ description: 'Text of publication', nullable: false })
     @IsString()
     text: string;
+    @ApiProperty({ description: 'User identifier', nullable: false })
     @IsString()
     userId: string;
 }
--- src/publication/publication.controller.ts
+++ src/publication/publication.controller.ts
@@ -18,12 +18,16 @@ import { PublicationService } from './publication.service';
 import { CreatePublicationDto } from './dto/create-publication.dto';
 import { PUBLICATION_NOT_FOUND_ERROR } from './publication.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Publications')
 @Controller('publication')
 export class PublicationController {
     constructor(private readonly publicationService: PublicationService) {}
 
     @Post('create')
+    @ApiOperation({ summary: 'Post a new publication' })
+    @ApiParam({ name: 'noteId', required: true, description: 'Note identifier' })
     @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe())
     async create(@Body() dto: CreatePublicationDto): Promise<PublicationModel> {
@@ -31,6 +35,8 @@ export class PublicationController {
     }
 
     @Patch(':id')
+    @ApiOperation({ summary: 'Updates a publication with specified id' })
+    @ApiParam({ name: 'id', required: true, description: 'Publication identifier' })
     @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe())
     async patch(@Param('id') id: string, @Body() dto: CreatePublicationDto): Promise<void> {
@@ -42,6 +48,8 @@ export class PublicationController {
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Deletes a publication with specified id' })
+    @ApiParam({ name: 'id', required: true, description: 'Publication identifier' })
     @UseGuards(JwtAuthGuard)
     async delete(@Param('id') id: string): Promise<void> {
         const deletedPublication = await this.publicationService.delete(id);
@@ -52,12 +60,15 @@ export class PublicationController {
     }
 
     @Get('findAll')
+    @ApiOperation({ summary: 'Gets all publications' })
     @UseGuards(JwtAuthGuard)
     async findAll(): Promise<PublicationModel[]> {
         return this.publicationService.findAll();
     }
 
     @Get('findByUserId/:id')
+    @ApiOperation({ summary: 'Gets all publications with specified id' })
+    @ApiParam({ name: 'id', required: true, description: 'User identifier' })
     @UseGuards(JwtAuthGuard)
     async findByUserId(@Param('id') userId: string): Promise<PublicationModel[]> {
         return this.publicationService.findByUserId(userId);
