5b299a956391246c3f8ca646aa173c675e2df2c7
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-07 15:06:03 +0300

    feat: add guards to delete and post methods in comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -15,12 +15,13 @@ import { CommentModel } from './comment.model';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CommentService } from './comment.service';
 import { COMMENT_NOT_FOUND } from './comment.constants';
-import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('comment')
 export class CommentController {
     constructor(private readonly commentService: CommentService) {}
 
+    @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe())
     @Post('create')
     async create(@Body() dto: CreateCommentDto): Promise<CommentModel> {
@@ -33,6 +34,7 @@ export class CommentController {
         return this.commentService.findByPublicationId(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     async delete(@Param('id') id: string): Promise<void> {
         const deletedDoc = await this.commentService.delete(id);
