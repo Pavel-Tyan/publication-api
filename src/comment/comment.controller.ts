d8f83f0f5f8d4949508e58304b2c68b00bc2c155
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-07-07 20:22:56 +0300

    docs: add documentation for comment module

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -16,7 +16,9 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { CommentService } from './comment.service';
 import { COMMENT_NOT_FOUND } from './comment.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Comments')
 @Controller('comment')
 export class CommentController {
     constructor(private readonly commentService: CommentService) {}
@@ -24,18 +26,23 @@ export class CommentController {
     @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe())
     @Post('create')
+    @ApiOperation({ summary: 'Create a new comment' })
     async create(@Body() dto: CreateCommentDto): Promise<CommentModel> {
         return this.commentService.create(dto);
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('byPublication/:id')
+    @ApiOperation({ summary: 'Get comments by specified identifier of publication' })
+    @ApiParam({ name: 'id', required: true, description: 'Specified identifier of publication' })
     async getByPublication(@Param('id') id: string): Promise<CommentModel[]> {
         return this.commentService.findByPublicationId(id);
     }
 
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
+    @ApiOperation({ summary: 'Delete comments by specified identifier' })
+    @ApiParam({ name: 'id', required: true, description: 'Specified identifier of comment' })
     async delete(@Param('id') id: string): Promise<void> {
         const deletedDoc = await this.commentService.delete(id);
 
--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCommentDto {
+    @ApiProperty({ description: 'Comment text', nullable: false })
     @IsString()
     text: string;
+    @ApiProperty({ description: 'Specified id of user that created comment', nullable: false })
     @IsString()
     userId: string;
+    @ApiProperty({ description: 'Specified id of publication comment was created for', nullable: false })
     @IsString()
     publicationId: string;
 }
